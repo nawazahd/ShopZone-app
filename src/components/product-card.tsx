3bebf355606f262b9a72cf329d25c4b2e357c0f0
Nawaz Ahmed <ahmednawaz1810@gmail.com>
2024-02-23 19:25:23 +0530

    update

--- src/components/product-card.tsx
+++ src/components/product-card.tsx
@@ -1,5 +1,7 @@
 import { FaPlus } from "react-icons/fa";
 import { CartItem } from "../types/types";
+// import svgimg1 from "../../frontend/src/uploads/0f0898a0-bcc2-485b-8820-87c6e203d3ec.svg";
+import { server } from "../redux/store";
 
 type ProductsProps = {
   productId: string;
@@ -18,11 +20,11 @@ const ProductCard = ({
   stock,
   handler,
 }: ProductsProps) => {
-
-
+//D:\shopzone - Copy\frontend\src\assets\uploads\0f0898a0-bcc2-485b-8820-87c6e203d3ec.jpg
+//D:\shopzone - Copy\frontend\src\assets\cover.jpg
   return (
     <div className="product-card">
-      <img src={`https://raw.githubusercontent.com/nawazahd/ShopZone-app/master/src/assets/${photo}`} alt={name} />
+      <img src={`/src/assets/${photo}`} alt={name} />
       <p>{name}</p>
       <span>₹{price}</span>
 
